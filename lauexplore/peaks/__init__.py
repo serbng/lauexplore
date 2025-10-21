8dd2b7709e87aacef48e62cbbbbc6bfc5a2ac229
Sergio Bongiorno <101117801+serbng@users.noreply.github.com>
2025-10-21 16:22:02 +0200

    Comment only file that has a lauetools dependency
    Comment out the import of index and check_orientation functions.

--- lauexplore/peaks/__init__.py
+++ lauexplore/peaks/__init__.py
@@ -3,4 +3,4 @@ from ._duplicates import remove_duplicates
 from ._subtract import subtract
 from ._track import track
 from ._simulate import simulate
-from ._indexation import index, check_orientation
+# from ._indexation import index, check_orientation
